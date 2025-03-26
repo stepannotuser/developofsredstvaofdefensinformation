68593af916bd64578bddb45015e61c7e7cae0b12
stepannotuser <123289772+stepannotuser@users.noreply.github.com>
2025-03-26 15:17:35 +0700

    Update lab1_3.cpp
    The code has been changed in accordance with the teacher's suggestions when protecting the program.
    the changes are fixed in the comments

--- lab1_3.cpp
+++ lab1_3.cpp
@@ -37,9 +37,11 @@ public:
         return *logger;
     }
 
-    void SendMessage(const string& message) {
-        logger->Log("Load massage: " + message);
-        cout << "Message sent: " << message << endl;
+    void SendMessage(const string& message, int flag) {
+        if(flag){                                    // таким способом добавлен флаг, определяющий необходимость логгирования
+            logger->Log("Load massage: " + message);
+        }
+        cout << "Message sent: " << message << endl; // в любом случае пользователь получит сообщение
     }
 };
 
@@ -48,14 +50,19 @@ int main() {
     cout << endl <<endl;
 
 
-    FileLogger fileLogger;
-    DatabaseLogger dbLogger;
+    //FileLogger fileLogger;
+    //DatabaseLogger dbLogger;
 
-    SmtpMailer first(fileLogger);
-    SmtpMailer second(dbLogger);
+    //SmtpMailer first(fileLogger);
+    //SmtpMailer second(dbLogger);
 
-    first.SendMessage("Hello, world!");
-    second.SendMessage("This is a test message.");
+    //first.SendMessage("Hello, world!");
+    //second.SendMessage("This is a test message.");
+
+    ILogger* temp = new FileLogger();       // выделяю память в куче
+    SmtpMailer first(*temp);
+    first.SendMessage("Hello, world!", 1);  // путем флага устанавливаю необходимость логгирования, вне зависимости от получения сообщения
+    delete temp;                            // обязательно очищаю память
 
 
     cout << endl <<endl;
