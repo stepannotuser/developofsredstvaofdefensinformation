f99fc183365ad093f81a326f3806d3599d8cc588
stepannotuser <123289772+stepannotuser@users.noreply.github.com>
2025-03-30 12:48:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-
+class AbstractEntity(){
+}
+class AccountEntity : public AbstractEntity{
+}
+class RoleEntity : public AbstractEntity{
+}
+class Repository
+{
+  void Save(AbstractEntity entity)
+  { if (entity is AccountEntity){}
+    if (entity is RoleEntity){}
+  }
+}
