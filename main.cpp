212619595ec89034e5c0d98d1d7d8b01c11ea76f
stepannotuser <123289772+stepannotuser@users.noreply.github.com>
2025-03-31 20:06:51 +0700

    Update main.cpp
    smthing for it work

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
-class AbstractEntity(){
-}
-class AccountEntity : public AbstractEntity{
-}
-class RoleEntity : public AbstractEntity{
-}
-class Repository
-{
-  void Save(AbstractEntity entity)
-  { if (entity is AccountEntity){}
-    if (entity is RoleEntity){}
+class AbstractEntity {
+public:
+    virtual string GetName() const = 0;
+    virtual ~AbstractEntity() = default;
+};
+
+class AccountEntity : public AbstractEntity {
+public:
+    string GetName() const override {
+        return "AccountEntity";
+    }
+};
+
+class RoleEntity : public AbstractEntity {
+public:
+    string GetName() const override {
+        return "RoleEntity";
+    }
+};
+
+class IRepository {
+public:
+    virtual void Save(AbstractEntity* entity) = 0;
+    virtual ~IRepository() = default;
+};
+
+class AccountRepository : public IRepository {
+public:
+  void Save(AbstractEntity* entity) override {
   }
-}
+
+class RoleRepository : public IRepository {
+public:
+  void Save(RoleEntity* entity) override {
+  }
