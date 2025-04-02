3d63e5d7dc6a5c0756cc3e0b378994ed315fc0a0
stepannotuser <123289772+stepannotuser@users.noreply.github.com>
2025-04-02 10:14:43 +0700

    Update main.cpp
    3 commit
    tyt i dodelal main logiky code

--- main.cpp
+++ main.cpp
@@ -27,9 +27,38 @@ public:
 class AccountRepository : public IRepository {
 public:
   void Save(AbstractEntity* entity) override {
+    AccountEntity* account = dynamic_cast<AccountEntity*>(entity);
+    if (account != nullptr) {
+        cout << "Save object: " << account->GetName() << endl;
+    }     
   }
-
+};
 class RoleRepository : public IRepository {
 public:
   void Save(RoleEntity* entity) override {
+      RoleEntity* role = dynamic_cast<RoleEntity*>(entity);
+        if (role != nullptr) {
+            cout << "Save object: " << role->GetName() << endl;
+        }
   }
+};
+
+
+int main() {
+
+    AccountEntity account;
+
+    RoleEntity* role = new RoleEntity(); //
+
+    AccountRepository accountRepo;
+    RoleRepository roleRepo;             //
+
+    accountRepo.Save(&account);  
+    roleRepo.Save(role);                 //
+
+    accountRepo.Save(role); //
+
+    delete role;                         //
+
+    return 0;
+}
